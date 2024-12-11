02f6c598374bdc9eec311bfe60c4d31f0a6c5102
Valentin Loginov <valentin.loginov@deliveryhero.com>
2024-12-11 06:36:33 +0100

    feat: day 11

--- app/src/test/kotlin/aoc2024/d11.kt
+++ app/src/test/kotlin/aoc2024/d11.kt
@@ -2,5 +2,5 @@ package aoc2024
 
 class D11Test: SolutionTest<Long>(
     solution = D11(),
-    answers = Answers(null, null, null, null),
+    answers = Answers(55312, 216042, 65601038650482, 255758646442399),
 ) {}
---
+++ app/src/test/kotlin/aoc2024/d12.kt
@@ -0,0 +1,6 @@
+package aoc2024
+
+class D12Test: SolutionTest<Long>(
+    solution = D12(),
+    answers = Answers(null, null, null, null),
+) {}
